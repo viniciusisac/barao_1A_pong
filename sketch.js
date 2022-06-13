52f197047fc06a702699ef54d0a05a27dece43a1
@vineee_06 <107411085+viniciusisac@users.noreply.github.com>
2022-06-13 11:16:19 -0300

    Add files via upload
    23 mai 2022, 09:53:44

---
+++ sketch.js
@@ -0,0 +1,12 @@
+let xBolinha = 100;
+let yBolinha = 200;
+let dianetro = 22;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  circle(xBolinha, yBolinha, dianetro)
+}
